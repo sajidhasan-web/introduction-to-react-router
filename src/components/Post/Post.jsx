dc1ebea22a3d60fac7f969fa8b8b81c135ccf695
sajidhasan-web <sajidhasan.web@gmail.com>
2024-03-20 14:48:51 +0600

    add click show details button and also go back button

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,8 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 const Post = ({post}) => {
     const {id, title}=post
+
+    const navigate = useNavigate()
+
+  const  handleShowDetails = () =>{
+    navigate(`/post/${id}`)
+  }
+
+
+
    
     return (
         <div style={{border:'2px solid green', padding: '15px', borderRadius:'15px'}}>
@@ -10,6 +19,7 @@ const Post = ({post}) => {
             <p>{title}</p>
             <Link to={`/post/${id}`}>Post detail</Link>
             <Link to={`/post/${id}`}><button>Show details</button></Link>
+            <button onClick={handleShowDetails}>Show details by Click</button>
         </div>
     );
 };
--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,14 +1,22 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 
 const PostDetails = () => {
     const post = useLoaderData();
     const {id, title, body } = post;
+
+    const navigate = useNavigate()
+
+    const handleGoBack = () =>{
+        navigate(-1)
+    }
+
     return (
         <div>
             <h3>Post details about: {id}</h3>
             <p>Title: {title}</p>
             <p><small>{body}</small></p>
+            <button onClick={handleGoBack}>Go Back</button>
             
         </div>
     );
